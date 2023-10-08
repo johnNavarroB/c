64b82fb043788eb02b73d1dcc67a97073b24cb01
d <d@d.d>
2023-10-08 18:31:42 +0200

    Added ft_memset to libft.h and Makefile

--- proj/libft/ft_memset.c
+++ proj/libft/ft_memset.c
@@ -1,24 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jonnavar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/01 15:19:12 by jonnavar          #+#    #+#             */
-/*   Updated: 2023/10/01 15:25:14 by jonnavar         ###   ########.fr       */
+/*   Created: 2023/10/02 17:55:21 by jonnavar          #+#    #+#             */
+/*   Updated: 2023/10/02 17:56:00 by jonnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef LIBFT_H
-# define LIBFT_H
+#include <stdlib.h>
 
-int		ft_isalpha(char c);
-int		ft_isdigit(char c);
-int		ft_isalnum(char c);
-int		ft_isascii(char c);
-int		ft_isprint(char c);
+void	*ft_memset(void *b, int character, size_t length)
+{
+	int	index;
 
-int		ft_strlen(char c);
-void	*ft_memset(void *b, int character, size_t length);
-
-#endif
+	index = 0;
+	while (index < (int) length)
+	{
+		((char *) b)[index] = character;
+		index ++;
+	}
+	return (b);
+}
